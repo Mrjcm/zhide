c75e594921ab33b2848b8846beba3cc1b5e12306
mrjcm <461679353@qq.com>
2017-08-08 18:06:42 +0800

    增加律师详情

--- js/m_zhide.js
+++ js/m_zhide.js
@@ -6,6 +6,26 @@ $(function(){
 		}else{
 		      $(".nav").hide('fast');     //如果元素为显现,则将其隐藏
 		}
-	})
+	}).end();
+
+	// 职业机会
+	$(".jobList h5").on('click',function(){
+		if ($(this).siblings('.recruit_require').is(":hidden") ) {
+			$(this).siblings('.recruit_require').show('fast');
+			$(this).css({'borderColor':'#fff'});
+			$(this).parent('.outShow').css({'borderColor':'#C81432'});
+			$(this).parent('.outShow').siblings('.outShow').css({'borderColor':'#fff'});
+			$(this).parent('.outShow').siblings('.outShow').children('.recruit_require').hide('fast');
+			$(this).parent('.outShow').siblings('.outShow').children('h5').css({'borderColor':'#C4AE78'});
+		}else{
+			$(this).siblings('.recruit_require').hide('fast');
+			$(this).css({'borderColor':'#C4AE78'});
+			$(this).parent('.outShow').css({'borderColor':'#fff'});
+		}
+	}).end();
 // -jcm-
 })
+	/*------------------高德地图--------------*/
+     //创建和初始化地图函数：
+   
+	/*---------------------------------------------------*/
